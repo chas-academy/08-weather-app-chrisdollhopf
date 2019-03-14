32d3dd1fe587b3bedb57b223c9f9a5768036d5a4
Chris <christopher.dollhopf@chasacademy.se>
2019-03-14 13:44:28 +0100

    first commit, api fetch, logging of temp, humidity and wind speed

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    temperature: [],
+    humidity: [],
+    windSpeed: []
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+    }
+
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e2d3921448cf0e66aa6e4aed72a26abf/37.8267,-122.4233?units=si"
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          temperature: data.currently.temperature,
+          humidity: data.currently.humidity,
+          windSpeed: data.currently.windspeed
+        });
+        console.log(this.state);
+        console.log(data);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
-            <h1>Weather App</h1>
-
+          <h1>Weather App</h1>
+          <p>temperature: {this.state.temperature}</p>
+          <p>humidity: {this.state.humidity}</p>
+          <p>windspeed: {this.state.windspeed}</p>
+          <p />
         </header>
       </div>
     );
